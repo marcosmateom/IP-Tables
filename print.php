14179ac3089a2dacd70c80142d6e11a751fbe297
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-09-05 12:04:42 -0600

    prueba de comando escrito a mano

--- print.php
+++ print.php
@@ -1,4 +1,11 @@
+<form action="print.php">
+    <label>Comando</label>
+    <input type="text" id="comando" name="comando">
+    <button type="submit"> correr comando</button>
+</form>
+
 <?php
-$output = exec('php -v');
- echo $output;
+$comand = $_GET['comando'];
+$output = exec($comand);
+echo $output;
 ?>
