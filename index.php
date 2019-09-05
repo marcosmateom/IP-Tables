80085f609f2438e503561a76ea915e3afed22d4f
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-09-05 12:47:28 -0600

    Iniciando a trabajar con el index

--- index.php
+++ index.php
@@ -6,10 +6,14 @@
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 </head>
 <body>
-    <h1>Menu principal</h1>
-    <hr>
-    <?php
-        echo "<br><a href='print.php'>link a pruebas de impresion</a>";
-    ?>
+    <div class="container">
+        <h1>Menu principal</h1>
+        <hr>
+        <a href='#' class='button'>Nueva regla</a>
+        <?php
+            echo "<br><a class='button' href='print.php'>link a pruebas de impresion</a>";
+        ?>
+    </div>
+    
 </body>
 </html>
