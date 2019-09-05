6b6bc385f383f96fb52432cc800095785096e950
Miguel Gonzalez <miguelgon_8@hotmail.com>
2019-09-05 12:39:18 -0600

    update styles

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
- echo "Estas en el index";
- echo "<br><a href='print.php'>link a pruebas de impresion</a>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <h1>Menu principal</h1>
+    <hr>
+    <?php
+        echo "<br><a href='print.php'>link a pruebas de impresion</a>";
+    ?>
+</body>
+</html>
