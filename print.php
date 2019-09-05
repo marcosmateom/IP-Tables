84211911ea930a6429efddfb9e48046b47961736
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-09-05 14:48:52 -0600

    ppp

--- print.php
+++ print.php
@@ -5,7 +5,14 @@
 </form>
 
 <?php
+
+echo '<h3>Chain OUTPUT</h3>';
+	$var2 = shell_exec('sudo iptables -nL OUTPUT');
+	echo "<pre>$var2</pre>";
+	echo '<br><br>';
+
 $comand = $_GET['comando'];
+
 //$output = shell_exec("sudo iptables -L");
 $output = shell_exec($comand);
 //$output = suexec($comand);
