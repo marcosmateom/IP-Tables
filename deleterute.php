2ec91844ef172fdf27a872c7c8938f88f7eb1441
root <miguelgon_8@hotmail.com>
2019-09-05 17:52:16 -0600

    delete

--- deleterute.php
+++ deleterute.php
@@ -18,6 +18,7 @@
             <div class="form-group">
             <label for="sel1">CADENA</label>
             <select name="chain" class="form-control" id="sel1">
+                <option value=""></option>
                 <option value="INPUT">INPUT</option>
                 <option value="OUTPUT">OUTPUT</option>
                 <option value="FORWARD">FORWARD</option>    
@@ -27,6 +28,7 @@
             <div class="form-group">
             <label for="sel2">TARGET</label>
             <select name="target" class="form-control" id="sel2">
+                <option value=""></option>
                 <option value="ACCEPT">ACCEPT</option>
                 <option value="DROP">DROP</option>
                 <option value="RETURN">RETURN</option>    
@@ -36,15 +38,51 @@
             <div class="form-group">
             <label for="sel3">PARAMETROS</label>
             <select name="params" class="form-control" id="sel3">
-                <option value="-i">RED DE INTERFAZ ENTRANTE</option>
-                <option value="-p">PROTOCOLO</option>
-                <option value="-s">FUENTE PARA PAQUETE</option>    
+                <option value=""></option>
+                <option value="-i">RED DE INTERFAZ ENTRANTE -i</option>
+                <option value="-p">PROTOCOLO -p</option>
+                <option value="-s">FUENTE PARA PAQUETE -s</option>    
+            </select>
+            </div>
+            
+
+            <div class="form-group">
+            <label for="sel4">PROTOCOLOS</label>
+            <select name="protocols" class="form-control" id="sel4">
+                <option value=""></option>
+                <option value="tcp">TCP</option>
+                <option value="udp">UDP</option>
+            </select>
+            </div>
+
+            <div class="form-group">
+            <label for="sel5">TIPO DE PUERTO</label>
+            <select name="porttype" class="form-control" id="sel5">
+                <option value=""></option>
+                <option value="--dport">DPORT</option>
+                <option value="--sport">SPORT</option>
             </select>
             </div>
             
             <div class="form-group">
+            <label for="sel6">PARAMETROS2</label>
+            <select name="params2" class="form-control" id="sel6">
+                <option value=""></option>
+                <option value="-d">DESTINATION HOSTNAME -d</option>
+                <option value="-o">OUTGOING NETWORK INTERFACE -o</option>
+                  
+            </select>
+            </div>
+
+            <div class="form-group">
                 <label for="ipip">IP</label>
                 <input type="text" name="ip" class="form-control" id="ipip" placeholder="IP">
+                
+            </div>
+
+            <div class="form-group">
+                <label for="port">PORT</label>
+                <input type="text" name="port" class="form-control" id="ports" placeholder="PUERTO">
                 
             </div>
             
