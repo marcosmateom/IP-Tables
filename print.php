07211348b008295cd3529585662f625d46e3b855
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-09-05 17:50:28 -0600

    m

--- print.php
+++ print.php
@@ -14,7 +14,7 @@
 	//echo "<pre>$var2</pre>";
     echo '<br><br>';
     
-    echo '<table style="width:100%">';
+    echo '<table>';
     $heads = explode( " ", $lines[1] );
     echo '<tr>';
     foreach ($heads as $head) {
