3cdaa0849ab41f818b3a635db713fcc145656c25
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-09-05 16:21:27 -0600

    p

--- print.php
+++ print.php
@@ -11,22 +11,8 @@ echo '<h3>Chain OUTPUT</h3>';
     $var2 = shell_exec('sudo iptables -L');
     $lines = explode( "\n", $var2 );
     foreach ($lines as $line) {
-        $spaces = substr_count($line, ' ');
-        if ($spaces < 5) {
-            echo $line;
-            echo '<br>';
-        } elseif ($spaces >= 5) {
-            $rules = explode( " ", $line );
-            echo '<tr>';
-            foreach ($rules as $rule) {
-                echo '<td>';
-                echo $rule;
-                echo '</td>';
-            }
-            echo '</tr>';
-        }
+        echo $line;
     }
-    echo '<br>';
     echo '<p>'. $var2.'</p>';
 	//echo "<pre>$var2</pre>";
 	echo '<br><br>';
