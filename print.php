20f94dbeb76082489fed3aede44007026a999337
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2019-09-05 17:03:54 -0600

    m

--- print.php
+++ print.php
@@ -6,32 +6,10 @@
 
 <?php
 
-echo '<h3>Chain OUTPUT</h3>';
     $comand = $_GET['comando'];
     $var2 = shell_exec('sudo iptables -L');
     $lines = explode( "\n", $var2 );
-    echo '<table>';
-    foreach ($lines as $line) {
-        $num = substr_count($line, ' ');
-            if ($num <5) {
-                echo $line;
-                echo '<br>';
-            }
-            if ($num >= 5) {
-                $line = trim($line);
-                $rules = explode( " ", $line );
-                echo '<tr>';
-                foreach ($rules as $rule) {
-                    echo '<tb>';
-                    echo $rule;
-                    echo '</tb>';
-                }
-                echo '</tr>';
-            }
-        
-
-    }
-    echo '</table>';
+    echo $lines[0];
     //echo '<p>'. $var2.'</p>';
 	//echo "<pre>$var2</pre>";
 	echo '<br><br>';
